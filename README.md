bf3e1758015f735805e47617ad0d5c276ef535e6
John Cole <johnwebbcole@gmail.com>
2017-02-26 15:42:24 -0600

    Converted to a gulp plugin.

--- README.md
+++ README.md
@@ -1,20 +1,27 @@
 # openjscad-standalone [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
-> A copy of OpenJsCad containing files for standalone usage.
+> A gulp plugin to create a github page from an OpenJsCad project.
 
-https://johnwebbcole.github.io/openjscad-standalone/
+https://johnwebbcole.github.io/gulp-openjscad-standalone/
 
 ## Installation
 
 ```sh
-$ npm install --save openjscad-standalone
+$ npm install --save gulp-openjscad-standalone gulp-load-plugins gulp-plumber
 ```
 
 ## Usage
 
 ```js
-var openjscadStandalone = require('openjscad-standalone');
+var gulp = require('gulp');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plugins = gulpLoadPlugins();
 
-openjscadStandalone('Rainbow');
+gulp.task('docs', function() {
+  return gulp.src('package.json')
+    .pipe(plugins.plumber())
+    .pipe(plugins['openjscad-standalone']())
+    .pipe(gulp.dest('docs'));
+});
 ```
 ## License
 
---
+++ index.js
@@ -0,0 +1,73 @@
+var through = require('through2');
+var fs = require('fs');
+var path = require('path');
+var gutil = require('gulp-util');
+var pkg = require('./package.json');
+var marked = require('marked');
+var _ = require('lodash');
+var template = _.template;
+
+function getFiles(stream) {
+  pkg.files.forEach(function (file) {
+    var filepath = path.resolve(__dirname, file);
+    var f = new gutil.File({
+      path: file.replace('docs/', ''),
+      contents: fs.readFileSync(filepath)
+    });
+    stream.push(f);
+  });
+}
+
+function renderIndex(stream, options) {
+
+  var filepath = path.resolve(__dirname, 'docs/index.tmpl');
+
+  var text = _.template(fs.readFileSync(filepath, 'utf8'))(options);
+  // var text = fs.readFileSync(filepath, 'utf8').toString();
+
+  var f = new gutil.File({
+    path: 'index.html',
+    contents: new Buffer(text)
+  });
+  stream.push(f);
+}
+
+function copyDist(stream, main) {
+  var filepath = path.resolve(__dirname, main);
+  var f = new gutil.File({
+    path: main.replace('dist/'),
+    contents: fs.readFileSync(filepath)
+  });
+  stream.push(f);
+}
+
+module.exports = function (options) {
+
+  var renderer = new marked.Renderer();
+  renderer.image = function (href, title, text) {
+    var newhref = href.replace('docs/', '');
+    return `<img src="${newhref}" alt="${text}" title="${title}">`;
+  };
+
+  return through.obj(function (file, encoding, callback) {
+    var self = this;
+    var target = JSON.parse(file.contents.toString());
+    var basepath = path.dirname(file.path);
+    options = Object.assign({
+      readme: marked(fs.readFileSync(path.resolve(basepath, 'README.md'), 'utf8'), {
+          renderer: renderer
+      }),
+      filename: path.resolve(basepath, target.main),
+      title: target.description,
+      version: target.version,
+      name: target.name,
+      theme:'<link rel="stylesheet" href="themes/architect/css/style.css" type="text/css">'
+    }, options);
+
+    console.log('options', options);
+    copyDist(self, options.filename);
+    getFiles(self);
+    renderIndex(self, options);
+    callback();
+  });
+};
